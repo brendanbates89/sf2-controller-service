a5e226323b2226856d3ab0958491372c2d9bbab4
Brendan Bates <brendanbates89@gmail.com>
2014-11-21 13:59:42 -0500

    Register the new Compiler Pass with the framework.

--- src/My/TestBundle/MyTestBundle.php
+++ src/My/TestBundle/MyTestBundle.php
@@ -3,7 +3,14 @@
 namespace My\TestBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use My\TestBundle\DependencyInjection\Compiler\ContainerAwarePass;
 
 class MyTestBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);        
+        $container->addCompilerPass(new ContainerAwarePass());
+    }
 }
